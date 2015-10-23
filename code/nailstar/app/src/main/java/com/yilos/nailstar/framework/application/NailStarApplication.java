12ee5067e0e8e25a25d8cb9a3cc2f08415a0620d
zhenghonglei <zhl123-123@163.com>
2015-10-23 18:30:26 +0800

    代码优化

--- code/nailstar/app/src/main/java/com/yilos/nailstar/framework/application/NailStarApplication.java
+++ code/nailstar/app/src/main/java/com/yilos/nailstar/framework/application/NailStarApplication.java
@@ -1,14 +1,22 @@
 package com.yilos.nailstar.framework.application;
 
+import android.graphics.Bitmap;
+
 import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiskCache;
 import com.nostra13.universalimageloader.cache.disc.naming.HashCodeFileNameGenerator;
 import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 import com.nostra13.universalimageloader.core.download.BaseImageDownloader;
 import com.nostra13.universalimageloader.utils.StorageUtils;
+import com.yilos.nailstar.R;
+import com.yilos.nailstar.framework.entity.NailStarApplicationContext;
+import com.yilos.nailstar.framework.exception.JSONParseException;
+import com.yilos.nailstar.framework.exception.NetworkDisconnectException;
+import com.yilos.nailstar.index.model.IndexServiceImpl;
 
 import java.io.File;
 
@@ -36,6 +44,16 @@ public class NailStarApplication extends android.app.Application {
         application = this;
 
         File cacheDir = StorageUtils.getCacheDirectory(getApplicationContext());
+
+        DisplayImageOptions options = new DisplayImageOptions.Builder()
+                .showImageOnLoading(R.mipmap.ic_launcher)
+                .resetViewBeforeLoading(false)  // default
+                .cacheInMemory(true) // default
+                .cacheOnDisk(true) // default
+                .imageScaleType(ImageScaleType.IN_SAMPLE_POWER_OF_2) // default
+                .bitmapConfig(Bitmap.Config.ARGB_8888) // default
+                .build();
+
         ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
                 .memoryCacheExtraOptions(480, 800) // default = device screen dimensions
                 .diskCacheExtraOptions(480, 800, null)
@@ -50,10 +68,21 @@ public class NailStarApplication extends android.app.Application {
                 .diskCacheFileCount(100)
                 .diskCacheFileNameGenerator(new HashCodeFileNameGenerator()) // default
                 .imageDownloader(new BaseImageDownloader(getApplicationContext())) // default
-                .defaultDisplayImageOptions(DisplayImageOptions.createSimple()) // default
+                .defaultDisplayImageOptions(options)
                 .writeDebugLogs()
                 .build();
+
         ImageLoader imageLoader = ImageLoader.getInstance();
         imageLoader.init(config);
+    }
+
+    public void preloadIndexContent() {
+        try {
+            NailStarApplicationContext.getInstance().setIndexContent(new IndexServiceImpl().getIndexContentFromNet());
+        } catch (NetworkDisconnectException e) {
+            //e.printStackTrace();
+        } catch (JSONParseException e) {
+            //e.printStackTrace();
+        }
     }
 }
--- code/nailstar/app/src/main/java/com/yilos/nailstar/index/entity/Poster.java
+++ code/nailstar/app/src/main/java/com/yilos/nailstar/index/entity/Poster.java
@@ -1,10 +1,12 @@
 package com.yilos.nailstar.index.entity;
 
+import java.io.Serializable;
+
 /**
  * Created by yangdan on 15/10/16.
  * 轮播图信息
  */
-public class Poster {
+public class Poster implements Serializable {
     /**
      * 图片地址
      */
@@ -24,6 +26,29 @@ public class Poster {
      *
      */
     private String activityUrl;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Poster poster = (Poster) o;
+
+        if (!picUrl.equals(poster.picUrl)) return false;
+        if (!topicId.equals(poster.topicId)) return false;
+        if (!type.equals(poster.type)) return false;
+        return !(activityUrl != null ? !activityUrl.equals(poster.activityUrl) : poster.activityUrl != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = picUrl.hashCode();
+        result = 31 * result + topicId.hashCode();
+        result = 31 * result + type.hashCode();
+        result = 31 * result + (activityUrl != null ? activityUrl.hashCode() : 0);
+        return result;
+    }
 
     public String getPicUrl() {
         return picUrl;
