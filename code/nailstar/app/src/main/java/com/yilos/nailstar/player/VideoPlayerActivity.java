8d20ad3913265b0ea481deaf4b9236e34f9a1b55
youhuan <2541833520@qq.com>
2015-10-24 10:41:23 +0800

    将sinavideo代码欠到widget中

--- code/nailstar/app/src/main/java/com/yilos/nailstar/player/VideoPlayerActivity.java
+++ code/nailstar/app/src/main/java/com/yilos/nailstar/player/VideoPlayerActivity.java
@@ -1,6 +1,5 @@
 package com.yilos.nailstar.player;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
@@ -16,8 +15,6 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
-import android.view.ViewGroup.MarginLayoutParams;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
