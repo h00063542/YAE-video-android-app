f2fdad078745a9212ac8de30af5e69e7ae21d8f5
zhaoguolee <zhaoguolee@163.com>
2015-10-27 19:02:39 +0800

    求教程代码上传

--- code/nailstar/app/src/main/java/com/yilos/nailstar/requirelession/view/LessionView.java
+++ code/nailstar/app/src/main/java/com/yilos/nailstar/requirelession/view/LessionView.java
@@ -1,7 +1,35 @@
 package com.yilos.nailstar.requirelession.view;
 
+import com.yilos.nailstar.requirelession.entity.CandidateLession;
+import com.yilos.nailstar.requirelession.entity.LessionActivity;
+
+import java.util.List;
+
 /**
  * Created by yilos on 15/10/27.
  */
 public interface LessionView {
+
+    /**
+     * 失败提示
+     */
+    void refreshFailed();
+
+    /**
+     * 刷新当前求教程活动
+     * @param lessionActivity
+     */
+    void refreshActivityTopic(LessionActivity lessionActivity);
+
+    /**
+     * 刷新投票页面
+     * @param voteLessionList
+     */
+    void refreshVoteLession(List<CandidateLession> voteLessionList);
+
+    /**
+     * 刷新投票页面
+     * @param rankingLessionList
+     */
+    void refreshRankingLession(List<CandidateLession> rankingLessionList);
 }
