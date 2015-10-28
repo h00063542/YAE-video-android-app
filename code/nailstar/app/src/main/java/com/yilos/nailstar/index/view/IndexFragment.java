a026e82cb9f4019a39450c38c60d756d93d8d618
zhenghonglei <zhl123-123@163.com>
2015-10-28 10:02:09 +0800

    添加到视频播放页面的链接

--- code/nailstar/app/src/main/java/com/yilos/nailstar/index/view/IndexFragment.java
+++ code/nailstar/app/src/main/java/com/yilos/nailstar/index/view/IndexFragment.java
@@ -2,6 +2,7 @@ package com.yilos.nailstar.index.view;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import com.yilos.nailstar.framework.entity.NailStarApplicationContext;
 import com.yilos.nailstar.index.entity.IndexContent;
 import com.yilos.nailstar.index.entity.Poster;
 import com.yilos.nailstar.index.presenter.IndexPresenter;
+import com.yilos.nailstar.player.VideoPlayerActivity;
 import com.yilos.nailstar.util.CollectionUtil;
 import com.yilos.widget.banner.Banner;
 import com.yilos.widget.pullrefresh.PullRefreshLayout;
@@ -118,6 +120,16 @@ public class IndexFragment extends Fragment implements IIndexView {
             for(Poster poster : posters){
                 ImageCacheView imageCacheView = new ImageCacheView(getActivity().getApplicationContext());
                 imageCacheView.setImageSrc(poster.getPicUrl());
+                imageCacheView.setClickable(true);
+                imageCacheView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getActivity(), VideoPlayerActivity.class);
+                        startActivity(intent);
+//                        SplashActivity.this.finish();
+//                        SplashActivity.this.overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                    }
+                });
                 views.add(imageCacheView);
             }
         }
