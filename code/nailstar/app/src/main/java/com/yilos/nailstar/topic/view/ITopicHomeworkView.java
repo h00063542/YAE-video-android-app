8b25e095344d63c4d958f90cf3db67ff2932908f
youhuan <2541833520@qq.com>
2015-11-10 19:19:49 +0800

    增加图文下载代码

---
+++ code/nailstar/app/src/main/java/com/yilos/nailstar/topic/view/ITopicHomeworkView.java
@@ -0,0 +1,8 @@
+package com.yilos.nailstar.topic.view;
+
+/**
+ * Created by yilos on 2015-11-05.
+ */
+public interface ITopicHomeworkView {
+    void afterSubmittedHomework(String newCommentId);
+}
