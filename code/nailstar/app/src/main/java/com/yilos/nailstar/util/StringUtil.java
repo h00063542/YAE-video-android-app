09cb2a894731ef3dc79e9e8daee8b27b47ef1ba2
youhuan <2541833520@qq.com>
2015-11-17 15:15:32 +0800

    增加评论功能

--- code/nailstar/app/src/main/java/com/yilos/nailstar/util/StringUtil.java
+++ code/nailstar/app/src/main/java/com/yilos/nailstar/util/StringUtil.java
@@ -27,6 +27,24 @@ public class StringUtil {
         return m.matches();
     }
 
+    public static String buildTelNumberHtmlText(String text) {
+        String commentContent = StringUtil.isEmpty(text) ? Constants.EMPTY_STRING : text;
+        ArrayList<String> phoneNumbers = findNumber(commentContent);
+        if (CollectionUtil.isEmpty(phoneNumbers)) {
+            return commentContent;
+        } else {
+            StringBuilder stringBuilder = new StringBuilder(text);
+            int index;
+            String number = null;
+            for (int i = 0, size = phoneNumbers.size(); i < size; i++) {
+                number = phoneNumbers.get(i);
+                index = commentContent.indexOf(number);
+                stringBuilder.replace(index, number.length() + index, "<a color=\"#529E84\" href=\"tel:" + number + "\">" + number + "</a>");
+            }
+            return stringBuilder.toString();
+        }
+    }
+
 
     public static ArrayList<String> findNumber(String str) {
         ArrayList<String> result = new ArrayList<String>();
