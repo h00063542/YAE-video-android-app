2e998f9a10ef62077c5c214e3febea83e47fe021
sisilai <1093132971@qq.com>
2015-11-24 11:44:53 +0800

    我的求教程列表

--- code/nailstar/app/src/main/java/com/yilos/nailstar/aboutme/requirelesson/view/RequireLessonListActivity.java
+++ code/nailstar/app/src/main/java/com/yilos/nailstar/aboutme/requirelesson/view/RequireLessonListActivity.java
@@ -1,7 +1,12 @@
 package com.yilos.nailstar.aboutme.requirelesson.view;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.widget.GridView;
 
 import com.yilos.nailstar.R;
 import com.yilos.nailstar.aboutme.model.LoginAPI;
@@ -9,7 +14,6 @@ import com.yilos.nailstar.aboutme.requirelesson.entity.RequireLesson;
 import com.yilos.nailstar.aboutme.requirelesson.presenter.RequireLessonPresenter;
 import com.yilos.nailstar.framework.view.BaseActivity;
 import com.yilos.widget.titlebar.TitleBar;
-import com.yilos.widget.view.MaxHeightGridLayoutManager;
 
 import java.util.ArrayList;
 
@@ -37,10 +41,8 @@ public class RequireLessonListActivity extends BaseActivity implements IRequireL
         titleBar.getTitleView(R.string.about_me_my_require_lesson);
         titleBar.getBackButton(this);
         recyclerView = (RecyclerView) findViewById(R.id.require_lesson_list);
-        MaxHeightGridLayoutManager maxHeightGridLayoutManager = new MaxHeightGridLayoutManager(this, 2, 500);
-        maxHeightGridLayoutManager.setOrientation(MaxHeightGridLayoutManager.VERTICAL);
-        maxHeightGridLayoutManager.setSmoothScrollbarEnabled(true);
-        recyclerView.setLayoutManager(maxHeightGridLayoutManager);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(gridLayoutManager);
         RequireLessonPresenter requireLessonPresenter = RequireLessonPresenter.getInstance(this);
         requireLessonPresenter.getRequireLessonList(LoginAPI.getInstance().getLoginUserId());
     }
