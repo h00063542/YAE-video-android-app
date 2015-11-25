8178b1ad650609cb88ce7d27d31ed87925a9fd81
zhenghonglei <zhl123-123@163.com>
2015-11-25 14:27:39 +0800

    优化图片加载效果

--- code/nailstar/widget/src/main/java/com/yilos/widget/view/ImageCacheView.java
+++ code/nailstar/widget/src/main/java/com/yilos/widget/view/ImageCacheView.java
@@ -2,7 +2,11 @@ package com.yilos.widget.view;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.View;
@@ -29,6 +33,8 @@ public class ImageCacheView extends ImageView implements ImageLoadingListener, I
 
     private OnClickListener clickListener;
 
+    private int loadPercent;
+
     public ImageCacheView(Context context) {
         this(context, null);
     }
@@ -40,7 +46,7 @@ public class ImageCacheView extends ImageView implements ImageLoadingListener, I
     public ImageCacheView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
 
-        setBackgroundColor(ContextCompat.getColor(getContext(), R.color.xk2));
+        setBackgroundColor(ContextCompat.getColor(getContext(), R.color.xk3));
 
         super.setOnClickListener(this);
     }
@@ -107,18 +113,55 @@ public class ImageCacheView extends ImageView implements ImageLoadingListener, I
 
     @Override
     protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
         if(loading) {
-            //canvas.drawBitmap();
-        } else if(!loadSuccess) {
+            Paint paint = new Paint();
+            paint.setColor(ContextCompat.getColor(getContext(), R.color.xk3));
+            canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), paint);
 
-        } else {
-            super.onDraw(canvas);
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_image_loading);
+            if(bitmap.getWidth() > canvas.getWidth() || bitmap.getHeight() > canvas.getHeight()) {
+                float widthScale = canvas.getWidth() / bitmap.getWidth();
+                float heightScale = canvas.getHeight() / bitmap.getHeight();
+                float scale = widthScale < heightScale ? widthScale : heightScale;
+                Matrix matrix = new Matrix();
+                matrix.postScale(scale, scale);
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+            }
+
+            float left = (canvas.getWidth() - bitmap.getWidth()) / 2;
+            float top = (canvas.getHeight() - bitmap.getHeight()) / 2;
+            canvas.drawBitmap(bitmap, left, top, paint);
+        }
+        if(!loading && !loadSuccess) {
+            Paint paint = new Paint();
+            paint.setColor(ContextCompat.getColor(getContext(), R.color.xk3));
+            canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), paint);
+
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_image_loading);
+            if(bitmap.getWidth() > canvas.getWidth() || bitmap.getHeight() > canvas.getHeight()) {
+                float widthScale = canvas.getWidth() / bitmap.getWidth();
+                float heightScale = canvas.getHeight() / bitmap.getHeight();
+                float scale = widthScale < heightScale ? widthScale : heightScale;
+                Matrix matrix = new Matrix();
+                matrix.postScale(scale, scale);
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+            }
+
+            float left = (canvas.getWidth() - bitmap.getWidth()) / 2;
+            float top = (canvas.getHeight() - bitmap.getHeight()) / 2;
+            canvas.drawBitmap(bitmap, left, top, paint);
+
+            paint.setColor(Color.BLACK);
+            int fontSize = getResources().getDimensionPixelSize(R.dimen.middle_text_size);
+            paint.setTextSize(fontSize);
+            canvas.drawText("点击重试加载", (canvas.getWidth() - 6 * fontSize) / 2, (canvas.getHeight() - fontSize) / 2 - bitmap.getHeight(), paint);
         }
     }
 
     @Override
     public void onProgressUpdate(String imageUri, View view, int current, int total) {
-
+        loadPercent = (current / total) * 100;
     }
 
     private void loadImage() {
