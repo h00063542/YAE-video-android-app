8e402cdaa519885d11b731b349f2b3fe5fd9f672
zhenghonglei <zhl123-123@163.com>
2015-11-27 18:07:00 +0800

    修复获取SD卡列表时出现异常的问题

--- code/nailstar/app/src/main/java/com/yilos/nailstar/util/SettingUtil.java
+++ code/nailstar/app/src/main/java/com/yilos/nailstar/util/SettingUtil.java
@@ -8,12 +8,16 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Environment;
 import android.os.StatFs;
+import android.os.storage.StorageManager;
 
 import com.tencent.bugly.crashreport.BuglyLog;
 import com.yilos.nailstar.R;
 import com.yilos.nailstar.aboutme.entity.Sdcard;
 import com.yilos.nailstar.framework.application.NailStarApplication;
 
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -92,8 +96,13 @@ public class SettingUtil {
         editor.commit();
     }
 
-    public static ArrayList<Sdcard> getSdcardList() {
-        ArrayList<Sdcard> sdcardArrayList = new ArrayList<>();
+    public static List<Sdcard> getSdcardList() {
+        List<Sdcard> sdcardArrayList = getCardListByHideMethod();
+        if(null != sdcardArrayList && sdcardArrayList.size() > 0) {
+            return sdcardArrayList;
+        }
+
+        sdcardArrayList = new ArrayList<>();
         if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
             Sdcard sdcard = new Sdcard();
             sdcard.setSdcardPath(Environment.getExternalStorageDirectory().getAbsolutePath());
@@ -116,6 +125,69 @@ public class SettingUtil {
         }
 
         return sdcardArrayList;
+    }
+
+    private static List<Sdcard> getCardListByHideMethod() {
+        StorageManager storageManager = (StorageManager)context.getSystemService(Context.STORAGE_SERVICE);
+
+        try {
+            Method getVolumePathsMethod = StorageManager.class.getMethod("getVolumePaths");
+            getVolumePathsMethod.setAccessible(true);
+
+            String[] paths = (String[])getVolumePathsMethod.invoke(storageManager);
+            if(null == paths || paths.length <= 0) {
+                return null;
+            }
+
+            ArrayList<Sdcard> sdcardArrayList = new ArrayList<>();
+            int cardCount = 1;
+            for(String path : paths) {
+                File filePath = new File(path);
+                boolean validate = false;
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    validate = filePath.exists()
+                            && Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState(filePath));
+                } else {
+                    validate = filePath.exists();
+                }
+                if(validate) {
+                    Sdcard sdcard = new Sdcard();
+                    sdcard.setSdcardPath(path);
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        if(Environment.isExternalStorageEmulated(filePath)) {
+                            sdcard.setSdcardName("手机存储");
+                        } else {
+                            sdcard.setSdcardName("SD存储卡" + cardCount++);
+                        }
+                    } else {
+                        if(Environment.isExternalStorageEmulated()
+                                && Environment.getExternalStorageDirectory() != null
+                                && Environment.getExternalStorageDirectory().getAbsolutePath().equals(path)) {
+                            sdcard.setSdcardName("手机存储");
+                        } else {
+                            sdcard.setSdcardName("SD存储卡" + cardCount++);
+                        }
+                    }
+
+                    initSdcardSize(sdcard);
+                    if(sdcard.getAvailCount() > 0) {
+                        sdcardArrayList.add(sdcard);
+                    }
+                }
+            }
+
+            return sdcardArrayList;
+        } catch (NoSuchMethodException e) {
+            BuglyLog.e("SettingUtil getCardListByHideMethod", "没有该方法getVolumePaths异常", e);
+        } catch (InvocationTargetException e) {
+            BuglyLog.e("SettingUtil getCardListByHideMethod", "调用方法getVolumePaths异常", e);
+        } catch (IllegalAccessException e) {
+            BuglyLog.e("SettingUtil getCardListByHideMethod", "没有权限调用该方法getVolumePaths异常", e);
+        } catch (Exception e) {
+            BuglyLog.e("SettingUtil getCardListByHideMethod", "未知异常异常", e);
+        }
+
+        return null;
     }
 
     private static void initSdcardSize(Sdcard sdcard) {
