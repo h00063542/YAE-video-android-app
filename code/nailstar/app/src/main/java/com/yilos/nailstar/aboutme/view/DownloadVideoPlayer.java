f7136beb8bb04280dd69e27ec55f254d624c7305
zhaoguolee <zhaoguolee@163.com>
2015-11-27 19:35:16 +0800

    更换播放器

--- code/nailstar/app/src/main/java/com/yilos/nailstar/aboutme/view/DownloadVideoPlayer.java
+++ code/nailstar/app/src/main/java/com/yilos/nailstar/aboutme/view/DownloadVideoPlayer.java
@@ -1,107 +1,62 @@
 package com.yilos.nailstar.aboutme.view;
 
-import android.content.res.Configuration;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.ViewGroup;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
+import android.view.WindowManager;
 
-import com.sina.sinavideo.sdk.VDVideoExtListeners;
-import com.sina.sinavideo.sdk.VDVideoView;
-import com.sina.sinavideo.sdk.data.VDVideoInfo;
-import com.sina.sinavideo.sdk.data.VDVideoListInfo;
+import com.android.tedcoder.wkvideoplayer.view.SuperVideoPlayer;
 import com.yilos.nailstar.R;
-import com.yilos.nailstar.framework.view.BaseActivity;
 
 /**
  * Created by yilos on 15/11/21.
  */
-public class DownloadVideoPlayer extends BaseActivity implements
-        VDVideoExtListeners.OnVDVideoPlaylistListener {
+public class DownloadVideoPlayer extends AppCompatActivity {
 
     public static final String TITLE = "title";
 
     public static final String URL = "url";
 
-    private VDVideoView mVDVideoView = null;
+    private SuperVideoPlayer mVDVideoView = null;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         setContentView(R.layout.download_video_player);
-        mVDVideoView = (VDVideoView) findViewById(R.id.vv1);
-        mVDVideoView.setVDVideoViewContainer((ViewGroup) mVDVideoView.getParent());
+        mVDVideoView = (SuperVideoPlayer) findViewById(R.id.vv1);
 
-        VDVideoListInfo infoList = new VDVideoListInfo();
-        VDVideoInfo info = new VDVideoInfo();
-        info.mTitle = getIntent().getStringExtra(TITLE);
-        info.mPlayUrl = getIntent().getStringExtra(URL);
-        infoList.addVideoInfo(info);
-
-        registerListener();
-
-        mVDVideoView.setIsFullScreen(true);
-        mVDVideoView.open(DownloadVideoPlayer.this, infoList);
-        mVDVideoView.play(0);
-
+        mVDVideoView.loadLocalVideo(getIntent().getStringExtra(URL));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mVDVideoView.onResume();
+        mVDVideoView.goOnPlay();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mVDVideoView.onPause();
-    }
-
-//    @Override
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        if (!mVDVideoView.onVDKeyDown(keyCode, event)) {
-//            return super.onKeyDown(keyCode, event);
-//        }
-//        return true;
-//    }
-
-    private void registerListener() {
-        mVDVideoView.setPlaylistListener(this);
+        mVDVideoView.pausePlay(false);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        mVDVideoView.onStop();
+        mVDVideoView.pausePlay(false);
     }
 
     @Override
     protected void onDestroy() {
-        mVDVideoView.release(false);
         super.onDestroy();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            mVDVideoView.setIsFullScreen(true);
-        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            mVDVideoView.setIsFullScreen(false);
-        }
-
-    }
-
-    @Override
-    public void onPlaylistClick(VDVideoInfo info, int p) {
-        if (info == null) {
-//            logger.error("onPlaylistClick info is null");
-        }
-        mVDVideoView.play(p);
+        mVDVideoView.close();
     }
 
 }
